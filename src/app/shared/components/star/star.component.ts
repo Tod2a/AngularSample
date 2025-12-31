db3fba698628b3526c4c673f0c83e7d01fe91a0e
Thomas Copin <thomas.copin@infrabel.be>
2025-12-31 10:03:15 +0100

    Adding event emitter output

--- src/app/shared/components/star/star.component.ts
+++ src/app/shared/components/star/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 @Component({
   selector: 'pm-star',
@@ -10,12 +10,13 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 export class StarComponent implements OnChanges {
   @Input({ required: true }) rating!: number;
   cropWidth: number = 75;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.cropWidth = this.rating * 75/5;
   }
 
   onClick(): void {
-    console.log(`The rating ${this.rating} was clicked!`);
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 }
