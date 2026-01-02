e9a4821b8b4fe9102ecb4ac9cd6730d7183658bf
Thomas Copin <thomas.copin@infrabel.be>
2026-01-02 16:31:53 +0100

    Adding guard to prevent bad navigation to product detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductListComponent } from './pages/products/product-list/product-list.component';
 import { ProductDetailComponent } from './pages/products/product-detail/product-detail.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { productDetailGuard } from './core/guards/product-detail.guard';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'products', component: ProductListComponent},
-    { path: 'products/:id', component: ProductDetailComponent},
+    { path: 'products/:id', canActivate: [productDetailGuard] ,component: ProductDetailComponent},
     { path: '', redirectTo: 'home', pathMatch:'full'},
     { path : '**', component: PageNotFoundComponent }
 ];
