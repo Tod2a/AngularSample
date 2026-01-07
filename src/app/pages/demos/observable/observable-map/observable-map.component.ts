1b747a9ba8731de435d82e770ff64034c9f7b3ae
Thomas Copin <thomas.copin@infrabel.be>
2026-01-07 15:43:30 +0100

    adding the map demo

--- src/app/pages/demos/observable/observable-map/observable-map.component.ts
+++ src/app/pages/demos/observable/observable-map/observable-map.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { of,from, map, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'pm-observable-map',
   standalone: true,
-  imports: [],
   templateUrl: './observable-map.component.html',
-  styleUrl: './observable-map.component.css'
+  styleUrl: './observable-map.component.css',
 })
-export class ObservableMapComponent {
+export class ObservableMapComponent implements OnInit, OnDestroy {
+  pageTitle: string = 'Observable Map';
 
+  apples$ = from([
+    { id: 1, type: 'macintosh' },
+    { id: 2, type: 'gala' },
+    { id: 3, type: 'pinklady' },
+  ]);
+  numbers$ = of(2, 4, 5, 6, 8);
+
+  subApples!: Subscription;
+  subNumbers!: Subscription;
+
+  ngOnInit(): void {
+    this.subApples = this.apples$
+      .pipe(
+        map(a => ({ ...a, color: 'red'}))
+      ).subscribe(x => console.log('Apple: ', x));
+    
+    this.subNumbers = this.numbers$
+      .pipe(
+        map(n => n * 2)
+      ).subscribe(x => console.log('Number: ', x));
+  }
+
+  ngOnDestroy(): void {
+    this.subApples.unsubscribe();
+    this.subNumbers.unsubscribe();
+  }
 }
