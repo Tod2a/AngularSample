2adc5c0e54d74b2682185bf2e9d03dc96dc25885
Thomas Copin <thomas.copin@infrabel.be>
2026-01-08 14:14:50 +0100

    Implement the tap example

--- src/app/pages/demos/observables/observable-tap/observable-tap.component.ts
+++ src/app/pages/demos/observables/observable-tap/observable-tap.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { of,from, Subscription, tap } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -6,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './observable-tap.component.html',
   styleUrl: './observable-tap.component.css'
 })
-export class ObservableTapComponent {
+export class ObservableTapComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Observable Tap';
+
+  apples$ = from([
+      { id: 1, type: 'macintosh' },
+      { id: 2, type: 'gala' },
+      { id: 3, type: 'pinklady' },
+    ]);
+    numbers$ = of(2, 4, 5, 6, 8);
+  
+    subApples!: Subscription;
+    subNumbers!: Subscription;
+  
+    ngOnInit(): void {
+      this.subApples = this.apples$
+        .pipe(
+          tap(a => console.log('Apple: ', a))
+        ).subscribe();
+      
+      this.subNumbers = this.numbers$
+        .pipe(
+          tap(a => console.log('Number: ', a))
+        ).subscribe();
+    }
+  
+    ngOnDestroy(): void {
+      this.subApples.unsubscribe();
+      this.subNumbers.unsubscribe();
+    }
 }
